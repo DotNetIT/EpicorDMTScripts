ffffe092a80fed3205bee413f4a57d1114f45e7c
A9G-Data-Droid <akauffman@cet.aero>
2021-09-03 09:31:02 -0700

    Insights 2017: DMT PowerShell
    Update from Insights 2017: DMT PowerShell

--- PowerShell/1-DMTLoad.ps1
+++ PowerShell/1-DMTLoad.ps1
@@ -1,10 +1,10 @@
-﻿#DMT Automation Example 1
+#DMT Automation Example 1
 
 $DMTPath = "C:\Epicor\ERP10\LocalClients\ERP10\DMT.exe"
 $User = "epicor"
 $Pass = "epicor"
 $Source = "C:\Temp\Product.csv"
- 
 
 #Load Data
+
 Start-Process -Wait -FilePath $DMTPath -ArgumentList "-User $User -Pass $Pass -Delete -Import Part -Source $Source "
--- PowerShell/3-DBExtractAndLoad.ps1
+++ PowerShell/3-DBExtractAndLoad.ps1
@@ -1,17 +1,19 @@
-﻿#DMT Automation Example 3
+#DMT Automation Example 3
 
 #Extract Data From DB -> CSV File -> Load in with DMT
 
 Import-module "sqlps" -DisableNameChecking
+
 $DMTPath = "C:\Epicor\ERP10\LocalClients\ERP10\DMT.exe"
 $User = "epicor"
 $Pass = "epicor"
-
 $Source = "C:\Temp\Product.csv"
 $completeLog = $source + ".CompleteLog.txt"
 
 Write-Output "Extracting Data"
+
 #Extract Data
+
 Invoke-SqlCmd -Query "SELECT TOP 50 
                         'EPIC06' As Company,
                         ProductNumber As PartNum,
@@ -21,12 +23,13 @@ Invoke-SqlCmd -Query "SELECT TOP 50
 
 
 Write-Output "Extracted : " Import-Csv $Source | Measure-Object | % {$_.Count}
-
 Write-Output "Loading Data " $Source 
 
 #Load Data
+
 Start-Process -Wait -FilePath $DMTPath -ArgumentList "-User $User -Pass $Pass -Add -Update -Import Part -Source $Source "
 
 #Check Results
+
 select-string -Path $completeLog -Pattern "Records:\D*(\d+\/\d+)" -AllMatches | % { $_.Matches.Groups[0].Value }
 select-string -Path $completeLog -Pattern "Errors:\D*(\d+)" -AllMatches | % { $_.Matches.Groups[0].Value }
